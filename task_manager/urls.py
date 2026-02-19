84be73858975ab491f4364cb000955c66c250cc4
Егор Аношин <egoranoshin@MacBook-Pro-Egor.local>
2026-02-19 12:35:36 +0300

    fix: remove django_select2 references

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("tasks.urls")),
-    path("select2/", include("django_select2.urls")),
 ]
